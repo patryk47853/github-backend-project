794ab99e6d8b55acdbe2bf7ff857e30dc9b97b94
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-02-16 14:55:56 +0100

    Initial commit

---
+++ src/main/java/pl/patrykjava/GithubBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBackendProjectApplication.class, args);
+	}
+
+}
